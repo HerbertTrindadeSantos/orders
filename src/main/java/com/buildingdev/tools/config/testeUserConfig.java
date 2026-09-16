e2d2cdfe4406f9fef0613f6c5bbdfdd1d849fd13
HerbertTrindade-dev <herbert.santos77@gmail.com>
2026-09-16 16:16:10 -0300

    criacao de repositoy e testeConfig para injecao no DB

---
+++ src/main/java/com/buildingdev/tools/config/testeUserConfig.java
@@ -0,0 +1,27 @@
+package com.buildingdev.tools.config;
+
+import com.buildingdev.tools.entities.User;
+import com.buildingdev.tools.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class testeUserConfig implements CommandLineRunner {
+
+    private UserRepository userRepository;
+
+    public testeUserConfig(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        userRepository.saveAll(Arrays.asList(u1,u2));
+    }
+}
