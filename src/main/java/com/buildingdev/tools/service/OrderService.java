68342f57467f4e5ab31013d2f4186e2c477d126c
HerbertTrindade-dev <herbert.santos77@gmail.com>
2026-09-17 14:23:03 -0300

    Criacao de classe order e Repository,service e controller

---
+++ src/main/java/com/buildingdev/tools/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.buildingdev.tools.service;
+
+import com.buildingdev.tools.entities.Order;
+import com.buildingdev.tools.exception.OrderNotFoundException;
+import com.buildingdev.tools.repositories.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository){
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+
+    public Order findById(long id){
+        Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException("Pedido nao encontrado id: "+id));
+        return order;
+    }
+}
