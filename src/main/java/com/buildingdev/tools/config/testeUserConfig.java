a3829ca2d226d92fd1082dbe539893f704f5d134
HerbertTrindade-dev <herbert.santos77@gmail.com>
2026-09-25 13:43:13 -0300

    Alterando testConfig para funcionar na base da prod

--- src/main/java/com/buildingdev/tools/config/testeUserConfig.java
+++ src/main/java/com/buildingdev/tools/config/testeUserConfig.java
@@ -11,7 +11,7 @@ import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
-@Profile("test")
+@Profile("prod")
 public class testeUserConfig implements CommandLineRunner {
 
     private UserRepository userRepository;
